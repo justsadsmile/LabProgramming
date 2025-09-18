a738c392f19528d5caec31b3b9c9ef8f20401cfd
Smile <giveme5starbaton@gmail.com>
2025-09-18 19:37:24 +0400

    Add files via upload

---
+++ 1st-year/basic-programming/lab-1-math-formula/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace mathFormula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Constant and variables
+            const double pi = Math.PI;
+            const double e = Math.E;
+            string stgA;
+            string stgB;
+            double dResult;
+
+            //Input and Parse
+            Console.Write("Enter a:");//Input a
+            stgA = Console.ReadLine();
+            double a = double.Parse(stgA);
+
+            Console.Write("Enter b:");//Input b
+            stgB = Console.ReadLine();
+            double b = double.Parse(stgB);
+
+            //Calculation
+            double fUp = Math.Pow(Math.Cos(pi), 7) + Math.Sqrt(Math.Log(Math.Pow(b, 4), e));
+            double fDown = Math.Sin(Math.Pow(((pi / 2) + a), 2));
+            dResult = fUp / fDown;
+
+            //Output
+            string output = string.Format("{0:f2}", dResult);
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ 1st-year/programming-workshop/lab-1-bmi-calculator/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace calculatorBMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //the threshold for a normal BMI for an adult
+            const double minBMI = 18.5;
+            const double maxBMI = 24.9;
+
+            //data input
+            Console.Write("Enter weight(kg):");
+            string stgWeight = Console.ReadLine();
+            double weight = double.Parse(stgWeight);
+
+            Console.Write("Enter height(m):");
+            string stgHeight = Console.ReadLine();
+            double height = double.Parse(stgHeight);
+
+            //calculation
+            double a;
+            a = Math.Pow(height, 2);
+            a = weight / a;
+
+            //formatting the result
+            string formatA = string.Format("{0:f2}", a);
+
+            //output the result
+            Console.WriteLine(formatA);
+            if (a <= minBMI)//output the interpretation
+            {
+                Console.WriteLine("Flaw");
+            }
+            else if (a >= maxBMI)
+            {
+                Console.WriteLine("Overweight");
+            }
+            else
+            {
+                Console.WriteLine("Norm");
+            }
+        }
+    }
+}
